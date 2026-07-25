2fc411e591d08ac7860cb795713e71e71212d247
韩多龙 <17793630527@163.com>
2026-07-25 11:23:02 +0800

    更新工程

---
+++ jzo2o-api/src/main/java/com/jzo2o/api/customer/AddressBookApi.java
@@ -0,0 +1,13 @@
+package com.jzo2o.api.customer;
+
+import com.jzo2o.api.customer.dto.response.AddressBookResDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//contextId 指定FeignClient实例的上下文id，如果不设置默认为类名，value指定微服务的名称，path:指定接口地址
+@FeignClient(contextId = "jzo2o-customer", value = "jzo2o-customer", path = "/customer/inner/address-book")
+public interface AddressBookApi {
+    @GetMapping("/{id}")
+    AddressBookResDTO detail(@PathVariable("id") Long id);
+}
